4fc0d98135714c4c78038250979490c1e62180b7
Tazim <tazim0877@gmail.com>
2025-01-24 00:32:43 +0600

    async handler added

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 import express from "express";
-
+import { LIMIT } from "./constants.js";
 const app = express();
 
-app.use(express.json());
+// app config
+app.use(express.json({ limit: LIMIT }));
+app.use(express.urlencoded({ limit: LIMIT }));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.json({
--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,2 @@
 export const DB_NAME = "naivedb";
+export const LIMIT = "64kb";
