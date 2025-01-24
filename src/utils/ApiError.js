3ab11b428191b6c4f1a089dfe248525b681b4429
Tazim <tazim0877@gmail.com>
2025-01-24 17:59:27 +0600

    ApiError Wrapper added

---
+++ src/utils/ApiError.js
@@ -0,0 +1,23 @@
+class ApiError extends Error {
+  constructor(
+    status,
+    message = "something went wrong!",
+    errors = [],
+    success = false
+  ) {
+    super(message);
+    this.status = status;
+    this.message = message;
+    this.success = success;
+    this.errors = errors;
+  }
+
+  toJSON() {
+    return {
+      status: this.status,
+      message: this.message,
+      error: this.errors,
+      success: this.success,
+    };
+  }
+}
