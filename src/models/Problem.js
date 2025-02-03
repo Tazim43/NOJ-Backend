ae8f4e60cd2b8470b388475d78f176d5a80003f0
Tazim <tazim0877@gmail.com>
2025-02-03 19:18:03 +0600

    Problem schema created

---
+++ src/models/Problem.js
@@ -0,0 +1,67 @@
+import mongoose from "mongoose";
+
+const ProblemSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: [true, "title is required"],
+    },
+    statementId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "ProblemStatement",
+      required: [true, "statementId is required"],
+    },
+    tags: [
+      {
+        type: String,
+      },
+    ],
+    difficulty: {
+      type: String,
+      enum: ["Easy", "Medium", "Hard"],
+    },
+    authorId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "authorId is required"],
+    },
+    solveCount: {
+      type: Number,
+      default: 0,
+    },
+    isVisible: {
+      type: Boolean,
+      default: false,
+    },
+    timeLimit: {
+      type: Number,
+      required: [true, "timeLimit is required"],
+    },
+    memoryLimit: {
+      type: Number,
+      required: [true, "memoryLimit is required"],
+    },
+    isContestProblem: {
+      type: Boolean,
+      default: false,
+    },
+    contestId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Contest",
+    },
+    attempts: {
+      type: Number,
+      default: 0,
+    },
+    acceptanceRate: {
+      type: Number,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Problem", ProblemSchema);
