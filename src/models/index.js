4b19c3d721bee2e807a531707c8371e24055e5ee
Tazim <tazim0877@gmail.com>
2025-02-04 04:08:04 +0600

    exporting all models from one file

--- src/models/index.js
+++ src/models/index.js
@@ -1,7 +1,29 @@
 import User from "./User";
 import Problem from "./Problem";
+import Solution from "./Solution";
+import Contest from "./Contest";
+import Announcement from "./Announcement";
+import Clarification from "./Clarification";
+import ContestLeaderboard from "./ContestLeaderboard";
+import Feedback from "./Feedback";
+import ProblemStatement from "./ProblemStatement";
+import Submission from "./Submission";
+import Tag from "./Tag";
+import Validator from "./Validator";
+import Testcase from "./Testcase";
 
 module.exports = {
   User,
   Problem,
+  Solution,
+  Contest,
+  Announcement,
+  Clarification,
+  ContestLeaderboard,
+  Feedback,
+  ProblemStatement,
+  Submission,
+  Tag,
+  Testcase,
+  Validator,
 };
