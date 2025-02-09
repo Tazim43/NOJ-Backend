8da8aa4e0b6611152e777990a43c41dab5f7e728
Tazim <tazim0877@gmail.com>
2025-02-10 03:45:20 +0600

    Admin controller added

---
+++ src/controllers/Admin.js
@@ -0,0 +1,68 @@
+import { asyncHandler } from "../utils/asyncHandler";
+
+exports.getAllUsers = asyncHandler(async (req, res) => {
+  res.json({
+    msg: "TODO",
+  });
+});
+
+exports.getUserByID = asyncHandler(async (req, res) => {
+  res.json({
+    msg: "TODO",
+  });
+});
+
+exports.updateUser = asyncHandler(async (req, res) => {
+  res.json({
+    msg: "TODO",
+  });
+});
+
+exports.deleteUser = asyncHandler(async (req, res) => {
+  res.json({
+    msg: "TODO",
+  });
+});
+
+exports.getUserActivity = asyncHandler(async (req, res) => {
+  res.json({
+    msg: "TODO",
+  });
+});
+
+exports.deactivateUser = asyncHandler(async (req, res) => {
+  res.json({
+    msg: "TODO",
+  });
+});
+
+exports.activateUser = asyncHandler(async (req, res) => {
+  res.json({
+    msg: "TODO",
+  });
+});
+
+exports.rejudgeProblemById = asyncHandler(async (req, res) => {
+  res.json({
+    msg: "TODO",
+  });
+});
+
+exports.rejudgeContestById = asyncHandler(async (req, res) => {
+  res.json({
+    msg: "TODO",
+  });
+});
+
+// TODO: Implement the stats controller
+exports.stats = asyncHandler(async (req, res) => {
+  res.json({
+    msg: "TODO",
+  });
+});
+
+exports.maintainence = asyncHandler(async (req, res) => {
+  res.json({
+    msg: "TODO",
+  });
+});
