09e626389c330d067b997435b63b64e83920a244
Tazim <tazim0877@gmail.com>
2025-02-13 18:55:40 +0600

    bcrypt password hashing added before saving into db

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const UserSchema = new mongoose.Schema(
   {
@@ -113,5 +114,19 @@ const UserSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
+
+UserSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    try {
+      const salt = await bcrypt.genSalt(10);
+      this.password = await bcrypt.hash(this.password, salt);
+      next();
+    } catch (err) {
+      next(err);
+    }
+  } else {
+    next();
+  }
+});
 
 export default mongoose.model("User", UserSchema);
