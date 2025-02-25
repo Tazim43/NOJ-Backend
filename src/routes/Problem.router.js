b06525605050cadd1625c804d6263c3e0e09fa15
Tazim <tazim0877@gmail.com>
2025-02-26 00:47:10 +0600

    added all routes for problem controller

--- src/routes/Problem.router.js
+++ src/routes/Problem.router.js
@@ -2,22 +2,49 @@ import express from "express";
 import {
   getAllProblems,
   createProblem,
+  createProblemStatement,
   getProblemById,
+  getProblemStatementById,
   updateProblem,
+  updateProblemStatement,
   deleteProblem,
   updateProblemVisibility,
 } from "../controllers/Problem.controller.js";
 
+import {
+  authenticate,
+  authorizeProblemAuthor,
+} from "../middlewares/auth.middleware.js";
+import { upload } from "../middlewares/upload.middleware.js";
+import multerError from "../middlewares/multerError.js";
+
 const router = express.Router();
 
-router.route("/").get(getAllProblems).post(createProblem); // user role
+// Main Route : BASE_URL/problems
+
+// get all problems : public, create problem : user
+router.route("/").get(getAllProblems).post(authenticate, createProblem);
 
 router
   .route("/:id")
-  .get(getProblemById)
-  .put(updateProblem) // user role
-  .delete(deleteProblem); // user role
+  .get(getProblemById) // get problem by id : public
+  .put(authenticate, authorizeProblemAuthor, updateProblem) // update problem : author
+  .delete(authenticate, authorizeProblemAuthor, deleteProblem); // delete problem : author
 
-router.route("/:id/visibility").put(updateProblemVisibility); // user role
+router
+  .route("/:id/statement")
+  .get(authenticate, authorizeProblemAuthor, getProblemStatementById) // QQ : is it necessary? | for testing purpose
+  .post(
+    upload.array("imageList", 5),
+    authenticate,
+    authorizeProblemAuthor,
+    createProblemStatement,
+    multerError
+  )
+  .put(authenticate, authorizeProblemAuthor, updateProblemStatement);
+
+router
+  .route("/:id/visibility")
+  .put(authenticate, authorizeProblemAuthor, updateProblemVisibility); // update problem visibility : author
 
 export default router;
