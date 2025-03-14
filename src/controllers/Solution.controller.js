bd2ec42a60481aa2d2be4733d371b76c9b08f329
Tazim <tazim0877@gmail.com>
2025-03-14 16:41:01 +0600

    Fix : remove base64Decode & check solution exists
    - removed base64Decode for input, frontend will handle it
    - added check for similar solutions while creating a new one

--- src/controllers/Solution.controller.js
+++ src/controllers/Solution.controller.js
@@ -5,7 +5,7 @@ import { solutionValidation } from "../validation/problemAssetsValidation.js";
 import Solution from "../models/Solution.js";
 import ApiError from "../utils/apiError.js";
 import axios from "axios";
-import { base64Decode, base64Encode } from "../utils/Base64.js";
+import { base64Decode } from "../utils/base64.js";
 import { fetchCompilerOutput } from "../utils/fetchSubmission.js";
 
 // @desc      Get all solutions of a problem
@@ -30,10 +30,6 @@ const getSolution = asyncHandler(async (req, res) => {
 
     const solutionIds = problem.solutionIds;
     const solutions = await Solution.find({ _id: { $in: solutionIds } });
-
-    for (let i = 0; i < solutions.length; i++) {
-      solutions[i].source_code = base64Decode(solutions[i].source_code);
-    }
 
     res.status(StatusCodes.OK).json({
       status: StatusCodes.OK,
@@ -61,106 +57,69 @@ const createSolution = asyncHandler(async (req, res) => {
   // 7. check if the submission is successful
   // 8. add the sol id to the problem
   // 9. save the solution to the database
+  // 10. send the response
+
+  let { source_code, languageId } = req.body;
+  const problemId = req.params.id;
+
+  if (!source_code || !languageId) {
+    throw new ApiError(
+      StatusCodes.BAD_REQUEST,
+      ReasonPhrases.BAD_REQUEST,
+      "source_code and language_id are required"
+    );
+  }
+
+  const problem = await Problem.findById(problemId);
+  if (!problem) {
+    throw new ApiError(
+      StatusCodes.NOT_FOUND,
+      ReasonPhrases.NOT_FOUND,
+      "Problem not found"
+    );
+  }
+
+  // check the solution already exists
+  const existingSolution = await Solution.findOne({
+    problemId,
+  })
+    .where("source_code")
+    .equals(source_code)
+    .where("languageId")
+    .equals(languageId);
+  if (existingSolution) {
+    throw new ApiError(
+      StatusCodes.BAD_REQUEST,
+      ReasonPhrases.BAD_REQUEST,
+      "Solution already exists"
+    );
+  }
+
+  // submit the source code to the compiler and get the token
+
+  const axoisCreateOptions = {
+    method: "POST",
+    url: `${process.env.CEE_URI}/submissions`,
+    params: {
+      base64_encoded: "true",
+      wait: "false",
+    },
+    headers: {
+      "Content-Type": "application/json",
+      "x-rapidapi-host": process.env.CEE_API_HOST,
+      "x-rapidapi-key": process.env.CEE_API_KEY,
+    },
+    data: {
+      source_code,
+      language_id: languageId,
+    },
+  };
+
+  let token = "";
 
   try {
-    // 1. get the source code and language id from the request body
-    let { source_code, languageId } = req.body;
-    const problemId = req.params.id;
-
-    if (!source_code || !languageId) {
-      throw new ApiError(
-        StatusCodes.BAD_REQUEST,
-        ReasonPhrases.BAD_REQUEST,
-        "source_code and language_id are required"
-      );
-    }
-
-    source_code = base64Encode(source_code);
-
-    const problem = await Problem.findById(problemId);
-    if (!problem) {
-      throw new ApiError(
-        StatusCodes.NOT_FOUND,
-        ReasonPhrases.NOT_FOUND,
-        "Problem not found"
-      );
-    }
-
-    // submit the source code to the compiler and get the token
-
-    const axoisCreateOptions = {
-      method: "POST",
-      url: `${process.env.CEE_URI}/submissions`,
-      params: {
-        base64_encoded: "true",
-        wait: "false",
-      },
-      headers: {
-        "Content-Type": "application/json",
-        "x-rapidapi-host": process.env.CEE_API_HOST,
-        "x-rapidapi-key": process.env.CEE_API_KEY,
-      },
-      data: {
-        source_code,
-        language_id: languageId,
-      },
-    };
-
-    let token = "";
-
-    try {
-      const response = await axios.request(axoisCreateOptions);
-      token = response.data.token;
-    } catch (error) {
-      throw new ApiError(
-        StatusCodes.INTERNAL_SERVER_ERROR,
-        ReasonPhrases.INTERNAL_SERVER_ERROR,
-        error.message
-      );
-    }
-
-    const result = await fetchCompilerOutput(token);
-
-    if (result.status !== StatusCodes.OK) {
-      throw new ApiError(
-        result.status,
-        "Compilation Error",
-        base64Decode(result.message)
-      );
-    }
-
-    // save the solution to the database
-
-    const solutionData = {
-      problemId,
-      languageId,
-      source_code,
-      authorId: req.user._id.toString(),
-    };
-
-    if (problem.solutionIds.length == 0) {
-      solutionData.isOptimal = true;
-    }
-    const validationResult = solutionValidation.safeParse(solutionData);
-    if (!validationResult.success) {
-      console.log("here");
-      throw new ApiError(
-        StatusCodes.BAD_REQUEST,
-        ReasonPhrases.BAD_REQUEST,
-        validationResult.error
-      );
-    }
-
-    const solution = await Solution.create(solutionData);
-
-    problem.solutionIds.push(solution._id);
-    await problem.save();
-
-    res.status(StatusCodes.CREATED).json({
-      status: StatusCodes.CREATED,
-      success: true,
-      data: solution,
-    });
+    const response = await axios.request(axoisCreateOptions);
+    token = response.data.token;
   } catch (error) {
     throw new ApiError(
       StatusCodes.INTERNAL_SERVER_ERROR,
@@ -168,6 +127,49 @@ const createSolution = asyncHandler(async (req, res) => {
       error.message
     );
   }
+
+  const result = await fetchCompilerOutput(token);
+
+  if (result.status !== StatusCodes.OK) {
+    throw new ApiError(
+      result.status,
+      "Compilation Error",
+      base64Decode(result.message)
+    );
+  }
+
+  // save the solution to the database
+
+  const solutionData = {
+    problemId,
+    languageId,
+    source_code,
+    authorId: req.user._id.toString(),
+  };
+
+  if (problem.solutionIds.length == 0) {
+    solutionData.isOptimal = true;
+  }
+  const validationResult = solutionValidation.safeParse(solutionData);
+  if (!validationResult.success) {
+    console.log("here");
+    throw new ApiError(
+      StatusCodes.BAD_REQUEST,
+      ReasonPhrases.BAD_REQUEST,
+      validationResult.error
+    );
+  }
+
+  const solution = await Solution.create(solutionData);
+
+  problem.solutionIds.push(solution._id);
+  await problem.save();
+
+  res.status(StatusCodes.CREATED).json({
+    status: StatusCodes.CREATED,
+    success: true,
+    data: solution,
+  });
 });
 
 // @desc      Get a single solution
@@ -187,7 +189,6 @@ const getSolutionById = asyncHandler(async (req, res) => {
       "Solution not found"
     );
   }
-  solution.source_code = base64Decode(solution.source_code);
 
   res.status(StatusCodes.OK).json({
     status: StatusCodes.OK,
@@ -217,12 +218,6 @@ const updateSolution = asyncHandler(async (req, res) => {
   let languageId = req.body?.languageId;
   if (!languageId) {
     languageId = solution.languageId;
-  }
-
-  if (source_code) {
-    source_code = base64Encode(source_code);
-  } else {
-    source_code = solution.source_code;
   }
 
   const validationResult = solutionValidation
