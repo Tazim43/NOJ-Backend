f3789247eb8b3117545d7091c8e6798eae239a08
Tazim <tazim0877@gmail.com>
2025-03-15 17:33:15 +0600

    add : verdict names added

--- src/constants.js
+++ src/constants.js
@@ -3,6 +3,9 @@ export const LIMIT = "64kb";
 export const BASEURL = "/api/v1";
 export const TESTCASE_LIMIT = 45;
 
+export const MAX_TIME_LIMIT = 20;
+export const MIN_MEMORY_LIMIT = 2048;
+
 export const ROLES = {
   ADMIN: "admin",
   USER: "user",
@@ -14,4 +17,17 @@ export const Languages = {
   CPP: 105,
   JAVA: 91,
   PYTHON: 100,
+};
+
+export const supportedLanguages = [103, 105, 91, 100];
+
+export const VERDICTS = {
+  AC: "ACCEPTED",
+  WA: "WRONG_ANSWER",
+  TLE: "TIME_LIMIT_EXCEED",
+  MLE: "MEMORY_LIMIT_EXCEED",
+  RE: "RUNTIME_ERROR",
+  CE: "COMPILATION_ERROR",
+  SKIPPED: "SKIPPED",
+  PENDING: "PENDING",
 };
